d9d632a1423f3825c671f4d04f79ed21db3b3f87
Munnaza <lukesh@daikimedia.com>
2026-08-13 16:21:02 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
       <JsonLd data={homeJsonLd} />
       <TopBar />
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
       <TourCategories />
       <PopularDestination />
       <PlanYourTrip />
